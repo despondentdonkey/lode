64be707c1a0252689548dfc3c4b8d28d67bc0c90
Parker Miller <despondentdonkey@gmail.com>
2014-07-31 16:45:14 -0600

    Refactor structure to allow LODE to be used as an AMD module.

--- lode.js
+++ lode.js
@@ -1,5 +1,20 @@
-var LODE = {
-    createAjaxRequest: function(fileName, type, callback) {
+// Copyright (c) 2013 Parker Miller
+// LODE may be freely distributed under the MIT license.
+
+(function(root, createLode) {
+    if (typeof define === 'function' && define.amd) {
+        define(createLode);
+    } else {
+        root.LODE = createLode();
+    }
+}(this, function() {
+
+    function File(path, type) {
+        this.path = path;
+        this.type = type;
+    }
+
+    function createAjaxRequest(fileName, type, callback) {
         var request = new XMLHttpRequest();
         request.open("GET", fileName, true);
         request.responseType = type;
@@ -20,85 +35,88 @@ var LODE = {
 
         return request;
     }
-};
 
-LODE.File = function(path, type) {
-    this.path = path;
-    this.type = type;
-};
+    //Loads Image and Audio assets that have been added.
+    function createLoader() {
+        'use strict';
+        var assets = [];
+        var loaded = 0;
 
-//Loads Image and Audio assets that have been added.
-LODE.createLoader = function() {
-    'use strict';
-    var assets = [];
-    var loaded = 0;
+        return {
+            loadImage: function(src) {
+                var newImage = new Image();
+                newImage.src = src;
+                assets.push(newImage);
+                return newImage;
+            },
 
-    return {
-        loadImage: function(src) {
-            var newImage = new Image();
-            newImage.src = src;
-            assets.push(newImage);
-            return newImage;
-        },
+            loadAudio: function(src) {
+                var newAudio = new Audio();
+                newAudio.src = src;
+                assets.push(newAudio);
+                return newAudio;
+            },
 
-        loadAudio: function(src) {
-            var newAudio = new Audio();
-            newAudio.src = src;
-            assets.push(newAudio);
-            return newAudio;
-        },
+            //Loads a file via ajax. Defaults to loading text.
+            loadFile: function(path, type) {
+                if (!type) {
+                    type = 'text';
+                }
+                var file = new File(path, type);
+                assets.push(file);
+                return file;
+            },
 
-        //Loads a file via ajax. Defaults to loading text.
-        loadFile: function(path, type) {
-            if (!type) {
-                type = 'text';
-            }
-            var file = new LODE.File(path, type);
-            assets.push(file);
-            return file;
-        },
+            //Call this when the document has been loaded. Specify a callback function to continue after the assets have been loaded.
+            load: function(callback) {
+                var onLoad = this.onLoad;
 
-        //Call this when the document has been loaded. Specify a callback function to continue after the assets have been loaded.
-        load: function(callback) {
-            var onLoad = this.onLoad;
+                var loadComplete = function(cb) {
+                    loaded++;
 
-            var loadComplete = function(cb) {
-                loaded++;
+                    //Meant to be overidden. Gives you the ratio of files loaded.
+                    if (onLoad) {
+                        onLoad(loaded / assets.length);
+                    }
 
-                //Meant to be overidden. Gives you the ratio of files loaded.
-                if (onLoad) {
-                    onLoad(loaded / assets.length);
+                    if (loaded >= assets.length) {
+                        cb();
+                    }
+                };
+
+                if (assets.length <= 0) {
+                    loadComplete(callback);
                 }
 
-                if (loaded >= assets.length) {
-                    cb();
-                }
-            };
-
-            if (assets.length <= 0) {
-                loadComplete(callback);
-            }
-
-            for (var i=0; i<assets.length; i++) {
-                var asset = assets[i];
-                if (asset instanceof Image) {
-                    asset.addEventListener('load', function() {
-                        loadComplete(callback);
-                    });
-                } else if (asset instanceof Audio) {
-                    asset.addEventListener('loadeddata', function() {
-                        loadComplete(callback);
-                    });
-                } else if (asset instanceof LODE.File) {
-                    (function(asset) { //Requires an anonymous function since asset is used in another callback.
-                        var request = LODE.createAjaxRequest(asset.path, asset.type, function(data) {
-                            asset.data = data;
+                for (var i=0; i<assets.length; i++) {
+                    var asset = assets[i];
+                    if (asset instanceof Image) {
+                        asset.addEventListener('load', function() {
                             loadComplete(callback);
                         });
-                        request.send();
-                    })(asset);
+                    } else if (asset instanceof Audio) {
+                        asset.addEventListener('loadeddata', function() {
+                            loadComplete(callback);
+                        });
+                    } else if (asset instanceof File) {
+                        (function(asset) { //Requires an anonymous function since asset is used in another callback.
+                            var request = createAjaxRequest(asset.path, asset.type, function(data) {
+                                asset.data = data;
+                                loadComplete(callback);
+                            });
+                            request.send();
+                        })(asset);
+                    }
                 }
             }
-        }
+        };
+    }
+
+    return {
+        VERSION: '0.1.0',
+        File: File,
+        createAjaxRequest: createAjaxRequest,
+        createLoader: createLoader,
     };
-};
+
+}));
